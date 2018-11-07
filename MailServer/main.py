b32ee2c862e1670920fa32c43029405378f1a1a0
Bos Mats <mats.bos@immalle.eu>
2018-11-07 11:44:36 +0100

    Refactored

---
+++ MailServer/main.py
@@ -0,0 +1,24 @@
+from MailServer import MailServer
+from MailClient import MailClient
+from MailItem import MailItem
+
+
+
+if __name__ == '__main__':
+    s = MailServer()
+    
+    print(" Logging in Joske to mail server...")
+    c1 = MailClient(s, "Joske")
+
+    print(" User joske sending mail to Jantje...")
+    c1.sendMailItem("Jantje", "Hello!!!")
+
+    print(" Logging in Jantje to mail server...")
+    c2 = MailClient(s, "Jantje")
+
+    print(" User Jantje reading mail...")
+    c2.printNextMailItem()
+
+    print(" User Jantje reading mail...")
+    c2.printNextMailItem()
+
